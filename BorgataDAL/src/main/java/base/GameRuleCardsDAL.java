efb5c47443f0f9f55411ce358953abf745c3bb93
osabahat <osabahat@Osman.fios-router.home>
2015-12-13 15:59:47 -0500

    added

---
+++ BorgataDAL/src/main/java/base/GameRuleCardsDAL.java
@@ -0,0 +1,88 @@
+package base;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import domain.GameRuleCardsDomainModel;
+import util.HibernateUtil;
+
+public class GameRuleCardsDAL   {
+
+	public static ArrayList<GameRuleCardsDomainModel> getCardsRules() {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction tx = null;
+		ArrayList<GameRuleCardsDomainModel> RuleCards = new ArrayList<GameRuleCardsDomainModel>();
+
+		try {
+			tx = session.beginTransaction();
+
+			List rules = session.createQuery("FROM GameRuleCardsDomainModel ORDER BY RULEID, PICKORDER").list();
+			for (Iterator iterator = rules.iterator(); iterator.hasNext();) {
+				GameRuleCardsDomainModel rle = (GameRuleCardsDomainModel) iterator.next();
+				RuleCards.add(rle);
+
+			}
+
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx != null)
+				tx.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return RuleCards;
+
+	}
+	// updates the rule if you decide you want to change it 
+
+	public static GameRuleCardsDomainModel updateRule(GameRuleCardsDomainModel grc){
+
+	Session session = HibernateUtil.getSessionFactory().openSession();
+
+	Transaction tx = null;
+
+	GameRuleCardsDomainModel grcGet = null;
+
+
+
+	try {
+
+	tx = session.beginTransaction();
+
+
+
+	session.update(grc);
+
+
+
+	tx.commit();
+
+	} catch (HibernateException e) {
+
+	if (tx != null)
+
+	tx.rollback();
+
+	e.printStackTrace();
+
+	} finally {
+
+	session.close();
+
+	}
+
+
+
+	return grc;
+
+	}
+
+	}
+	
+
